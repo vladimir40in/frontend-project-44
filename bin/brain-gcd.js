7ff87c19099c97e4b362fb350fb11fb150566f2f
vladimir40in <vladimirr40in@gmail.com>
2023-02-08 17:46:18 +0100

    Added Brain-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,23 @@
+import runGame from '../src/game.js';
+const gameDescription = 'Find the greatest common divisor of given numbers.';
+
+ var arrGame = [];
+ for (let i = 0; i < 3; i += 1) {
+
+    let firstNumber = Math.floor(Math.random() * 500);
+    let secondNumber = Math.ceil(Math.random() * 200);
+    const questionGreatestDivisor = `${firstNumber} ${secondNumber}`;
+    let greatestDivisor;
+
+        while (firstNumber !== secondNumber) {
+        if (firstNumber > secondNumber) { 
+        firstNumber = firstNumber - secondNumber;
+            }else{
+        secondNumber = secondNumber - firstNumber;
+        }
+    }
+    greatestDivisor = firstNumber;
+    arrGame.push({question: questionGreatestDivisor, answer:String(greatestDivisor)}); 
+};
+ 
+ runGame(arrGame, gameDescription);
