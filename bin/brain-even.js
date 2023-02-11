b0dca9b6ebdd027c07a869d3f356ce9884dacabb
vladimir40in <vladimirr40in@gmail.com>
2023-02-11 12:12:18 +0100

    Added Brain-prime

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,12 @@
 import runGame from '../src/game.js';
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no"';
+const isEven = (randomNumber) => (randomNumber % 2 === 0 ? 'yes' : 'no');
 
 var arrGame = [];
 for (let i = 0; i < 3; i += 1) {
-  const randomNumber = Math.floor(Math.random() * 100);
-  arrGame.push({question: randomNumber, answer: randomNumber % 2 === 0 ? 'yes' : 'no'}); 
-};
 
-runGame(arrGame, gameDescription);
+  const randomNumber = Math.floor(Math.random() * 100);
+    
+  arrGame.push({question: randomNumber, answer: isEven(randomNumber)}); 
+};
+  runGame(arrGame, gameDescription);
---
+++ bin/brain-prime.js
@@ -0,0 +1,19 @@
+  import runGame from '../src/game.js';
+  const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+  const isPrime = (checkingNumber) => {
+  
+     for (let j = 2; j < checkingNumber; j ++) { 
+       if (checkingNumber % j === 0) {
+        return 'no';
+       }
+     }
+        return 'yes';
+   };
+     var arrGame = [];
+      for (let i = 0; i < 3; i += 1) {
+const checkingNumber = Math.floor(Math.random() * 100);
+
+   arrGame.push({question: checkingNumber, answer: isPrime(checkingNumber)}); 
+};
+   runGame(arrGame, gameDescription);
