ea5b2d179a030d927e4dcd88e44f501ac4e1edc5
vladimir40in <vladimirr40in@gmail.com>
2023-02-12 12:51:52 +0100

    Brain-prime corrections

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -3,16 +3,16 @@ import runGame from '../src/game.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (checkingNumber) => {
-   for (let j = 2; j < checkingNumber; j += 1) {
-      if (checkingNumber % j === 0) {
+  for (let j = 2; j < checkingNumber; j += 1) {
+    if (checkingNumber % j === 0) {
       return 'no';
-      }
-   }
-   return 'yes';
+    }
+  }
+  return 'yes';
 };
 const arrGame = [];
 for (let i = 0; i < 3; i += 1) {
-   const checkingNumber = Math.floor(Math.random() * 100);
-   arrGame.push({ question: checkingNumber, answer: isPrime(checkingNumber) });
+  const checkingNumber = Math.floor(Math.random() * 100);
+  arrGame.push({ question: checkingNumber, answer: isPrime(checkingNumber) });
 }
 runGame(arrGame, gameDescription);
