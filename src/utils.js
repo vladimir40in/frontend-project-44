a8335cb2710c6553bf2d1139c23e67a46d6ff295
vladimir40in <vladimirr40in@gmail.com>
2023-02-27 17:14:02 +0100

    Refactoring #6

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,2 @@
-export const genRandomInteger = (min, max) => {
-   return Math.floor(Math.random() * (max - min + 1)) + min;
-};// min & max included
-
-export const square = (a, b) => {
-  return a * b;
-};
+export const genRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min; // min & max included
+export const square = (a, b) =>  a * b;
